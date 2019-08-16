41b6aab66728c99b1544b66ac81a6b4f2d3283e8
gaozhidong <1011284295@qq.com>
2019-08-16 10:41:10 +0800

    根据id查询详情接口

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -3,18 +3,23 @@ module.exports = app => {
   const router = express.Router()
   const Category = require('../../modules/Category')
 
-// 提交分类
+// 提交分类 create
   router.post('/categories', async (req, res) => {
     const model = await Category.create(req.body)
     res.send(model)
   })
 
-  // 获取分类列表
+  // 获取分类列表 find
   router.get('/categories',async (req,res)=>{
     const items = await Category.find(req.body)
     res.send(items)
   })
 
+  //获取详情 findById
+  router.get('/categories/:id',async (req,res)=>{
+    const model = await Category.findById(req.params.id)
+    res.send(model)
+  })
 
   app.use('/admin/api',router)
 }
