69601ad923eb0effd54603f4f92bba30aa44afbd
gaozhidong <1011284295@qq.com>
2019-08-16 13:58:09 +0800

    添加编辑更新接口

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -3,23 +3,29 @@ module.exports = app => {
   const router = express.Router()
   const Category = require('../../modules/Category')
 
-// 提交分类 create
+  // 提交分类 create
   router.post('/categories', async (req, res) => {
     const model = await Category.create(req.body)
     res.send(model)
   })
 
+  // 更新分类  findByIdAndUpdate
+  router.put('/categories/:id', async (req, res) => {
+    const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+    res.send(model)
+  })
+
   // 获取分类列表 find
-  router.get('/categories',async (req,res)=>{
+  router.get('/categories', async (req, res) => {
     const items = await Category.find(req.body)
     res.send(items)
   })
 
   //获取详情 findById
-  router.get('/categories/:id',async (req,res)=>{
+  router.get('/categories/:id', async (req, res) => {
     const model = await Category.findById(req.params.id)
     res.send(model)
   })
 
-  app.use('/admin/api',router)
+  app.use('/admin/api', router)
 }
