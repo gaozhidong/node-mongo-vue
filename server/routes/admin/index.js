1ebd57d25d9177d0efb656319d6a577aaaf2a09b
gaozhidong <1011284295@qq.com>
2019-08-17 10:17:59 +0800

    删除分类接口

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -14,6 +14,14 @@ module.exports = app => {
     const model = await Category.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
+  
+  //删除分类 findByIdAndDelete
+  router.delete('/categories/:id', async (req, res) => {
+    const model = await Category.findByIdAndDelete(req.params.id, req.body)
+    res.send({
+      success: true,
+    })
+  })
 
   // 获取分类列表 find
   router.get('/categories', async (req, res) => {
