03e47dca7dbdd61996f0f41f26d93fbbe53a86dd
gaozd <gaozd@253.com>
2019-08-18 20:40:11 +0800

    添加上级分类

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -3,6 +3,7 @@
   分类列表 
   <el-table :data="items">
     <el-table-column prop="_id" label="ID" width="320"></el-table-column>
+    <el-table-column prop="parent.name" label="上级分类"></el-table-column>
     <el-table-column prop="name" label="分类名称"></el-table-column>
     <el-table-column prop="createTime" label="创建时间"></el-table-column>
     <el-table-column label="操作">
