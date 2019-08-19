1344338b8985aea4557f7e5234b80b434829cd86
gaozhidong <1011284295@qq.com>
2019-08-19 10:08:25 +0800

    修改为通用型接口

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,39 +1,50 @@
 module.exports = app => {
   const express = require('express')
-  const router = express.Router()
-  const Category = require('../../modules/Category')
+  const router = express.Router({
+    mergeParams: true,
+  })
 
   // 提交分类 create
-  router.post('/categories', async (req, res) => {
-    const model = await Category.create(req.body)
+  router.post('/', async (req, res) => {
+    const modelName = require('inflection').classify(req.params.resource)
+    const Model = require(`../../modules/${modelName}`)
+    const model = await Model.create(req.body)
     res.send(model)
   })
 
   // 更新分类  findByIdAndUpdate
-  router.put('/categories/:id', async (req, res) => {
-    const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+  router.put('/:id', async (req, res) => {
+    const modelName = require('inflection').classify(req.params.resource)
+    const Model = require(`../../modules/${modelName}`)
+    const model = await Model.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
   
   //删除分类 findByIdAndDelete
-  router.delete('/categories/:id', async (req, res) => {
-    const model = await Category.findByIdAndDelete(req.params.id, req.body)
+  router.delete('/:id', async (req, res) => {
+    const modelName = require('inflection').classify(req.params.resource)
+    const Model = require(`../../modules/${modelName}`)
+    const model = await Model.findByIdAndDelete(req.params.id, req.body)
     res.send({
       success: true,
     })
   })
 
   // 获取分类列表 find
-  router.get('/categories', async (req, res) => {
-    const items = await Category.find().populate('parent').limit(10)
+  router.get('/', async (req, res) => {
+    const modelName = require('inflection').classify(req.params.resource)
+    const Model = require(`../../modules/${modelName}`)
+    const items = await Model.find().populate('parent').limit(10)
     res.send(items)
   })
 
   //获取详情 findById
-  router.get('/categories/:id', async (req, res) => {
-    const model = await Category.findById(req.params.id)
+  router.get('/:id', async (req, res) => {
+    const modelName = require('inflection').classify(req.params.resource)
+    const Model = require(`../../modules/${modelName}`)
+    const model = await Model.findById(req.params.id)
     res.send(model)
   })
 
-  app.use('/admin/api', router)
+  app.use('/admin/api/rest/:resource', router)
 }
