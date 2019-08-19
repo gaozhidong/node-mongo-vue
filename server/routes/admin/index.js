7103d13a2d6ba0c95f8c3f17b7484bdcfa7ef97f
gaozhidong <1011284295@qq.com>
2019-08-19 10:11:07 +0800

    使用中间件处理modelName

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -6,25 +6,19 @@ module.exports = app => {
 
   // 提交分类 create
   router.post('/', async (req, res) => {
-    const modelName = require('inflection').classify(req.params.resource)
-    const Model = require(`../../modules/${modelName}`)
-    const model = await Model.create(req.body)
+    const model = await req.Model.create(req.body)
     res.send(model)
   })
 
   // 更新分类  findByIdAndUpdate
   router.put('/:id', async (req, res) => {
-    const modelName = require('inflection').classify(req.params.resource)
-    const Model = require(`../../modules/${modelName}`)
-    const model = await Model.findByIdAndUpdate(req.params.id, req.body)
+    const model = await req.Model.findByIdAndUpdate(req.params.id, req.body)
     res.send(model)
   })
   
   //删除分类 findByIdAndDelete
   router.delete('/:id', async (req, res) => {
-    const modelName = require('inflection').classify(req.params.resource)
-    const Model = require(`../../modules/${modelName}`)
-    const model = await Model.findByIdAndDelete(req.params.id, req.body)
+    const model = await req.Model.findByIdAndDelete(req.params.id, req.body)
     res.send({
       success: true,
     })
@@ -32,19 +26,19 @@ module.exports = app => {
 
   // 获取分类列表 find
   router.get('/', async (req, res) => {
-    const modelName = require('inflection').classify(req.params.resource)
-    const Model = require(`../../modules/${modelName}`)
-    const items = await Model.find().populate('parent').limit(10)
+    const items = await req.Model.find().populate('parent').limit(10)
     res.send(items)
   })
 
   //获取详情 findById
   router.get('/:id', async (req, res) => {
-    const modelName = require('inflection').classify(req.params.resource)
-    const Model = require(`../../modules/${modelName}`)
-    const model = await Model.findById(req.params.id)
+    const model = await req.Model.findById(req.params.id)
     res.send(model)
   })
 
-  app.use('/admin/api/rest/:resource', router)
+  app.use('/admin/api/rest/:resource',async(req,res,next)=>{
+    const modelName = require('inflection').classify(req.params.resource)
+    req.Model = require(`../../modules/${modelName}`)
+    next()
+  }, router)
 }
