df55d1bb9cdfddce008be81b30c716e8a40ebc25
gaozhidong <1011284295@qq.com>
2019-08-20 10:39:20 +0800

    添加物品模型

---
+++ server/modules/Item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  name: { type: String },
+  icon: {type: String, },
+  createTime: {
+    type: Date,
+    default: Date.now
+  },
+  updateTime: {
+    type: Date,
+    default: Date.now
+  }
+}, { timestamps: { createdAt: 'createTime', updatedAt: 'updateTime' } })
+
+module.exports = mongoose.model('Item', schema)
