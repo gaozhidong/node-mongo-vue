74ec5531715e156285c184b7b72870eadd33fdd3
gaozhidong <1011284295@qq.com>
2019-08-20 10:35:30 +0800

    添加物品页面

---
+++ admin/src/views/ItemEdit.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h2>{{ id ? '编辑' : '创建' }}分类</h2>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="名称">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="图标">
+        <el-input v-model="model.icon"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {}
+  },
+  data() {
+    return {
+      model: {},
+    };
+  },
+  methods: {
+    async save() {
+      /* eslint-disable */
+      let res;
+
+      if (this.id) {
+        res = await this.$http.put(`rest/items/${this.id}`, this.model);
+      } else {
+        res = await this.$http.post("rest/items", this.model);
+      }
+
+      // console.log(res);
+      this.$router.push("/items/list");
+      this.$message({
+        type: "success",
+        message: "提交成功了"
+      });
+    },
+    async fetch() {
+      const res = await this.$http.get(`rest/items/${this.id}`);
+      this.model = res.data;
+    },
+  },
+  created() {
+    this.id && this.fetch();
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ admin/src/views/ItemList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+  分类列表 
+  <el-table :data="items">
+    <el-table-column prop="_id" label="ID" width="320"></el-table-column>
+    <el-table-column prop="parent.name" label="上级分类"></el-table-column>
+    <el-table-column prop="name" label="分类名称"></el-table-column>
+    <el-table-column prop="createTime" label="创建时间"></el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button type="text" @click="$router.push(`/categories/edit/${scope.row._id}`)">
+            操作
+        </el-button>
+        <el-button type="text" @click="remove(scope.row)">
+            删除
+        </el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items:[],
+      }
+    },
+    methods: {
+      async fetch(){
+        const res =  await this.$http.get('rest/categories')
+        this.items = res.data
+      },
+      async remove(row){
+        this.$confirm(`是否确认删除分类${row.name}`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(async() => {
+          const res = await this.$http.delete(`rest/categories/${row._id}`)
+          this.$message({
+            type: 'success',
+            message: '删除成功!',
+          });
+          this.fetch()
+        }).catch(() => {})
+      }
+    },
+    created() {
+      this.fetch()
+    },
+  }
+</script>
+
+<style>
+
+</style>
