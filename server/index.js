27a463ca95a7ae3ac8d547c311595657c421e311
gaozhidong <1011284295@qq.com>
2019-08-21 10:22:50 +0800

    使用express.static 进行静态文件托管

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const app = express()
 
 app.use(require('cors')())
 app.use(express.json())
+app.use('/uploads', express.static(__dirname + '/uploads'))
+
 require('./plugins/db')(app)
 require('./routes/admin')(app)
 
 
-app.listen(3333,()=>{
+app.listen(3333, () => {
   console.log('http://localhost:3333')
 })
