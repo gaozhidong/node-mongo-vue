44d00d0dccb86fc0a9749fbea3009443c4bac631
gaozhidong <1011284295@qq.com>
2019-08-23 13:57:24 +0800

    文章编辑和文章列表

---
+++ admin/src/views/ArticleEdit.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <h2>{{ id ? '编辑' : '创建' }}文章</h2>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="所属分类">
+        <el-select v-model="model.categories" multiple>
+          <el-option
+            v-for="item in categories"
+            :label="item.name"
+            :key="item._id"
+            :value="item._id"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="标题">
+        <el-input v-model="model.title"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {}
+  },
+  data() {
+    return {
+      model: {},
+      categories: []
+    };
+  },
+  methods: {
+    async save() {
+      /* eslint-disable */
+      let res;
+
+      if (this.id) {
+        res = await this.$http.put(`rest/articles/${this.id}`, this.model);
+      } else {
+        res = await this.$http.post("rest/articles", this.model);
+      }
+
+      // console.log(res);
+      this.$router.push("/articles/list");
+      this.$message({
+        type: "success",
+        message: "提交成功了"
+      });
+    },
+    async fetch() {
+      const res = await this.$http.get(`rest/articles/${this.id}`);
+      this.model = res.data;
+    },
+    async fetchCategories(){
+      const res = await this.$http.get(`rest/categories`);
+      this.categories = res.data;
+    }
+  },
+  created() {
+    this.fetchCategories()
+    this.id && this.fetch();
+  }
+};
+</script>
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
