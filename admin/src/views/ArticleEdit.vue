54ce357799e6c6e863076523bfa4d82e0a869434
gaozhidong <1011284295@qq.com>
2019-08-24 10:02:06 +0800

    使用vue2editor

--- admin/src/views/ArticleEdit.vue
+++ admin/src/views/ArticleEdit.vue
@@ -15,15 +15,22 @@
       <el-form-item label="标题">
         <el-input v-model="model.title"></el-input>
       </el-form-item>
+      <el-form-item label="详情">
+        <vue-editor v-model="model.body"></vue-editor>
+      </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
       </el-form-item>
-    </el-form>
+    </el-form>s
   </div>
 </template>
 
 <script>
+import { VueEditor } from "vue2-editor";
 export default {
+  components: {
+    VueEditor
+  },
   props: {
     id: {}
   },
@@ -55,13 +62,13 @@ export default {
       const res = await this.$http.get(`rest/articles/${this.id}`);
       this.model = res.data;
     },
-    async fetchCategories(){
+    async fetchCategories() {
       const res = await this.$http.get(`rest/categories`);
       this.categories = res.data;
     }
   },
   created() {
-    this.fetchCategories()
+    this.fetchCategories();
     this.id && this.fetch();
   }
 };
