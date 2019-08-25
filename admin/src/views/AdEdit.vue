5e6f0542f6526d2d8a455427fbfee3ebe52ce383
dong <1011284295@qq.com>
2019-08-25 10:59:30 +0800

    广告位的列表和编辑页面

---
+++ admin/src/views/AdEdit.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h2>{{ id ? '编辑' : '创建' }}广告位</h2>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      
+      <el-form-item label="名称">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>s
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {}
+  },
+  data() {
+    return {
+      model: {},
+    };
+  },
+  methods: {
+    async save() {
+      /* eslint-disable */
+      let res;
+
+      if (this.id) {
+        res = await this.$http.put(`rest/ads/${this.id}`, this.model);
+      } else {
+        res = await this.$http.post("rest/ads", this.model);
+      }
+
+      // console.log(res);
+      this.$router.push("/ads/list");
+      this.$message({
+        type: "success",
+        message: "提交成功了"
+      });
+    },
+    async fetch() {
+      const res = await this.$http.get(`rest/ads/${this.id}`);
+      this.model = res.data;
+    },
+  },
+  created() {
+    this.id && this.fetch();
+  }
+};
+</script>
