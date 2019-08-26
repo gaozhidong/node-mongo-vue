e1002c2c1785d564b0cdbf4e4457170c76023505
gaozhidong <1011284295@qq.com>
2019-08-26 10:36:32 +0800

    添加广告优化

--- admin/src/views/AdEdit.vue
+++ admin/src/views/AdEdit.vue
@@ -5,32 +5,32 @@
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
-      <el-form-item label="添加广告">
-       <el-button @click="model.items.push({})">
-            <i class="el-icon-plus"></i>
-            添加技能
-          </el-button>
-          <el-row type="flex" style="flex-wrap:wrap">
-            <el-col :md="24" v-for="(item,index) in model.items" :key="index">
-              <el-form-item label="跳转链接">
-                <el-input v-model="item.url"></el-input>
-              </el-form-item>
-              <el-form-item label="图片" style="margin-top:1rem">
-                <el-upload
-                  class="avatar-uploader"
-                  :action="$http.defaults.baseURL + '/upload'"
-                  :show-file-list="false"
-                  :on-success="res=>$set(item,'image',res.url)"
-                >
-                  <img v-if="item.image" :src="item.image" class="avatar" />
-                  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-                </el-upload>
-              </el-form-item>
-              <el-form-item>
-                <el-button size="small" type="danger" @click="model.items.splice(index,1)">删除</el-button>
-              </el-form-item>
-            </el-col>
-          </el-row>
+      <el-form-item label="添加广告" name="items">
+        <el-button @click="model.items.push({})">
+          <i class="el-icon-plus"></i>
+          添加广告
+        </el-button>
+        <el-row type="flex" style="flex-wrap:wrap">
+          <el-col :md="24" v-for="(item,index) in model.items" :key="index">
+            <el-form-item label="跳转链接">
+              <el-input v-model="item.url"></el-input>
+            </el-form-item>
+            <el-form-item label="图片" style="margin-top:1rem">
+              <el-upload
+                class="avatar-uploader"
+                :action="$http.defaults.baseURL + '/upload'"
+                :show-file-list="false"
+                :on-success="res=>$set(item,'image',res.url)"
+              >
+                <img v-if="item.image" :src="item.image" class="avatar" />
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+            </el-form-item>
+            <el-form-item>
+              <el-button size="small" type="danger" @click="model.items.splice(index,1)">删除</el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" native-type="submit">保存</el-button>
