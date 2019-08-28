a11be90f5b13dc5988db9100f0996ebc8bfa2a89
gaozhidong <1011284295@qq.com>
2019-08-28 10:54:59 +0800

    设置全局变量

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express()
-
+app.set('secret','abc')
 app.use(require('cors')())
 app.use(express.json())
 app.use('/uploads', express.static(__dirname + '/uploads'))
