e7910fa7ca8e344bda76d51891f1a0b44d383293
gaozhidong <1011284295@qq.com>
2019-08-28 11:00:49 +0800

    登录页面

--- admin/src/views/Login.vue
+++ admin/src/views/Login.vue
@@ -8,7 +8,7 @@
         <el-form-item label="密码">
           <el-input v-model="model.password"></el-input>
         </el-form-item>
-          <el-form-item>
+        <el-form-item>
           <el-button type="primary" native-type="submit">登录</el-button>
         </el-form-item>
       </el-form>
@@ -24,10 +24,16 @@ export default {
     };
   },
   methods: {
-    login(){
-      console.log(this.model)
+    async login() {
+      const res = await this.$http.post("login", this.model);
+      localStorage.token = res.data.token;
+      this.$router.push("/");
+      this.$message({
+        type: "success",
+        message: "登录成功"
+      });
     }
-  },
+  }
 };
 </script>
 
