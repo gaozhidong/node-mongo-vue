c536069b804e208e1539cc8b6728e80b499893b1
gaozhidong <1011284295@qq.com>
2019-08-29 10:19:22 +0800

    401 跳转到登录页面

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,29 +1,33 @@
 import axios from 'axios'
 import Vue from 'vue'
+import router from './router'
 const http = axios.create({
   baseURL: 'http://localhost:3333/admin/api'
 })
 
 
-http.interceptors.request.use((config)=>{
+http.interceptors.request.use((config) => {
 
-  if(localStorage.token){
-    config.headers.Authorization = "Bearer" + localStorage.token 
+  if (localStorage.token) {
+    config.headers.Authorization = "Bearer" + localStorage.token
   }
   return config
-},(err)=>{
+}, (err) => {
   return Promise.reject(err)
 })
 
 
-http.interceptors.response.use((res)=>{
+http.interceptors.response.use((res) => {
   return res
-}, err=>{
-  if(err.response.data.message){
+}, err => {
+  if (err.response.data.message) {
     Vue.prototype.$message({
-      type:'error',
+      type: 'error',
       message: err.response.data.message
     })
+  }
+  if (err.response.status === 401) {//如果未登录跳转到登录页面
+    router.push('/login')
   }
 
   return Promise.reject(err)
